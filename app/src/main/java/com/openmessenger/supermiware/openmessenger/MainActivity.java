e74496a161f44fee9c006cde2db9405ca920d284
Hamzah <supermiware@gmail.com>
2018-08-30 11:35:50 -0500

    Thanks

--- app/src/main/java/com/openmessenger/supermiware/openmessenger/MainActivity.java
+++ app/src/main/java/com/openmessenger/supermiware/openmessenger/MainActivity.java
@@ -25,22 +25,17 @@ import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.google.android.gms.common.internal.Constants;
+import com.android.volley.RequestQueue;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.ChildEventListener;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.IgnoreExtraProperties;
-import com.google.firebase.database.ValueEventListener;
+
 
 public class MainActivity extends AppCompatActivity {
-
     // Buttons
 
     private Button loginTabButton;
